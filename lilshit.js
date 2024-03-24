2ccf5be332986186717db09acfb2682621d8560e
aprilscreed <164689821+aprilscreed@users.noreply.github.com>
2024-03-23 21:20:36 -0500

    Delete lilshit.js

--- lilshit.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    let lilShit = document.getElementById('lilshit');
-    let x = e.clientX;
-    let y = e.clientY;
-    lilShit.style.left = x + 'px';
-    lilShit.style.top = y + 'px';
-});
